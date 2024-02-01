e5aac76ef3b9b02c7b8bf2a692cf59a03cfce937
LeudiX <leudix.rafael@gmail.com>
2024-02-01 05:15:07 -0500

    Tested new features Data Contract for CashCard List Serialization and Deserialization

--- src/test/java/com/example/demo/CashCardJsonTest.java
+++ src/test/java/com/example/demo/CashCardJsonTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -12,17 +13,34 @@ import java.io.IOException;
 /**
  * Starting with TDD (Test Driven Development) approach
  */
-@JsonTest /*Using the Jackson framework to provide extensive JSON testing and parsing support*/
+@JsonTest /*
+           * Using the Jackson framework to provide extensive JSON testing and parsing
+           * support
+           */
 public class CashCardJsonTest {
-    
-    @Autowired /*Create an object of the requested type specified below*/
-    private JacksonTester<CashCard> json; /*Wrapper to handle serialization and deserialization of JSON objects*/
-    
-    @Test
-    void cashCardSerializationTest() throws IOException{
-        CashCard cashcard = new CashCard(99L,123.45);
 
-        assertThat(json.write(cashcard)).isStrictlyEqualToJson("expected.json");
+    @Autowired /* Create an object of the requested type specified below */
+    private JacksonTester<CashCard> json; /* Wrapper to handle serialization and deserialization of JSON objects */
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonlist;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = new CashCard[] {
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 100.50),
+                new CashCard(101L, 325.33)
+        };
+    }
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashcard = cashCards[0];
+
+        assertThat(json.write(cashcard)).isStrictlyEqualToJson("single.json");
         assertThat(json.write(cashcard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashcard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
         assertThat(json.write(cashcard)).hasJsonPathNumberValue("@.amount");
@@ -43,4 +61,30 @@ public class CashCardJsonTest {
         assertThat(json.parseObject(expected).getId()).isEqualTo(99);
         assertThat(json.parseObject(expected).getAmount()).isEqualTo(123.45);
     }
+
+    /*Testing new GET features Data Contract */
+
+    /*
+     * Serialize the cashCards variable into JSON, then asserts that list.json
+     * should contain the same data as the serialized cashCards variable
+     */
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonlist.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
+
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+        String expected = """
+                [
+                    { "id": 99, "amount": 123.45 },
+                    { "id": 100, "amount": 100.50 },
+                    { "id": 101, "amount": 325.33 }
+                ]
+                        """;
+        assertThat(jsonlist.parse(expected)).isEqualTo(cashCards);
+        assertThat(jsonlist.parseObject(expected)[0].getId()).isEqualTo(99);
+        assertThat(jsonlist.parseObject(expected)[0].getAmount()).isEqualTo(123.45);
+    }
+
 }
