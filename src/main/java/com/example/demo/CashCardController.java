0b66c7500a1bb6f6a1798bd88100697a0875547b
LeudiX <leudix.rafael@gmail.com>
2024-01-31 23:41:36 -0500

    Added the POST method to the CashCard RESTController and the specific tests to apply TDD best practices to ensure the correct functioning of the API

--- src/main/java/com/example/demo/CashCardController.java
+++ src/main/java/com/example/demo/CashCardController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@RestController/*Spring component capable of handling HTTP requests*/
-@RequestMapping("/cashcards")/*Indicates which address requests must have to access this Controller*/
+@RestController /* Spring component capable of handling HTTP requests */
+@RequestMapping("/cashcards") /* Indicates which address requests must have to access this Controller */
 public class CashCardController {
 
     private final CashCardRepository cashCardRepository;
@@ -22,20 +24,32 @@ public class CashCardController {
         this.cashCardRepository = cashCardRepository;
     }
 
-    @GetMapping("/{requestedId}")/*marks a method as a handler method. GET requests that match cashcards/{requestedID} will be handled by this method*/
-    public ResponseEntity<CashCard> findById (@PathVariable Long requestedId){ /*makes Spring Web aware of the requestedId supplied in the HTTP request*/
+    @GetMapping("/{requestedId}") /*
+                                   * marks a method as a handler method. GET requests that match
+                                   * cashcards/{requestedID} will be handled by this method
+                                   */
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) { /*
+                                                                                 * makes Spring Web aware of the
+                                                                                 * requestedId supplied in the HTTP
+                                                                                 * request
+                                                                                 */
         Optional<CashCard> cashCard = cashCardRepository.findById(requestedId);
-        if(cashCard.isPresent()){
+        if (cashCard.isPresent()) {
             return ResponseEntity.ok(cashCard.get());
         } else
             return ResponseEntity.notFound().build();
     }
 
+    /* Saving the new CashCard and return its location. */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CashCard create(@RequestBody CashCard cashCard){
-        return cashCardRepository.save(cashCard);   
-        /*Saves the cashCard object to the database*/
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard cashCard, UriComponentsBuilder ucb) {
+
+        CashCard savedCashCard = cashCardRepository.save(cashCard);
+        /* Constructing a URI to the newly created CashCard */
+        URI cashCardLocation = ucb.path("/cashcards/{id}").buildAndExpand(savedCashCard.getId()).toUri();
+
+        return ResponseEntity.created(cashCardLocation).build();
     }
 
 }
--- src/test/java/com/example/demo/CashcardApplicationTests.java
+++ src/test/java/com/example/demo/CashcardApplicationTests.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.net.URI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
@@ -18,30 +19,71 @@ import com.jayway.jsonpath.JsonPath;
 class CashcardApplicationTests {
 
 	@Autowired
-	private TestRestTemplate restTemplate; /*Injecting a test helper that’ll allow us to make HTTP requests to the locally running application*/
+	private TestRestTemplate restTemplate; /*
+											 * Injecting a test helper that’ll allow us to make HTTP requests to the
+											 * locally running application
+											 */
 
 	@Test
-	void shouldReturnACashCardWhenDataIsSaved(){
-		/*Using restTemplate to make an HTTP GET request to our application endpoint /cashcards/99*/
-		ResponseEntity<String> response =  restTemplate.getForEntity("/cashcards/3", String.class);
-		
-		/*Expecting the HTTP reponse status code to be 200 OK*/
+	void shouldReturnACashCardWhenDataIsSaved() {
+		/*
+		 * Using restTemplate to make an HTTP GET request to our application endpoint
+		 * /cashcards/99
+		 */
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/3", String.class);
+
+		/* Expecting the HTTP reponse status code to be 200 OK */
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-		/*Converts the response String into a JSON-aware object with lots of helper methods*/
+		/*
+		 * Converts the response String into a JSON-aware object with lots of helper
+		 * methods
+		 */
 		DocumentContext dContext = JsonPath.parse(response.getBody());
-		
+
 		Number id = dContext.read("$.id");
-		Double amount  = dContext.read("$.amount");
+		Double amount = dContext.read("$.amount");
 		assertThat(id).isEqualTo(3);
 		assertThat(amount).isEqualTo(123.45);
 	}
 
 	@Test
-	void shouldNotReturnACashCardWithAnUnknownId(){
-		ResponseEntity<String> response =  restTemplate.getForEntity("/cashcards/1000", String.class);
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
 	}
 
+	@Test
+	void shouldCreateANewCashCard() {
+
+		/*
+		 * database will create and manage all unique CashCard.id values for us. We
+		 * shouldn't provide one
+		 */
+		CashCard cashCard = new CashCard(null, 100.0);
+
+		ResponseEntity<Void> response = restTemplate.postForEntity("/cashcards", cashCard, Void.class);
+
+		/*
+		 * Expecting the HTTP response status code to be 201 CREATED, which is
+		 * semantically
+		 * correct if our API creates a new CashCard from our request.
+		 */
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		/* Obtaninig the location of the recently created new CashCard resource through the Response Header */
+		URI locationOfNewCashCard = response.getHeaders().getLocation();
+		/*Performing a GET to access the newly created CashCard resource */
+		ResponseEntity<String> responseToNewCashCard = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+
+		assertThat(responseToNewCashCard.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext docContxt = JsonPath.parse(responseToNewCashCard.getBody());
+
+		Number id = docContxt.read("$.id");
+		Double amount = docContxt.read("$.amount");
+
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(100.0);
+	}
 }
