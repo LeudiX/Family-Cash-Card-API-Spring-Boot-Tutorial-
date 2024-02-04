b78ec1ebd30aaf803dc59c9823337f2f8379d355
LeudiX <leudix.rafael@gmail.com>
2024-02-04 01:26:10 -0500

    Updated README

--- src/main/java/com/example/demo/CashCardController.java
+++ src/main/java/com/example/demo/CashCardController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.apache.commons.logging.Log;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
